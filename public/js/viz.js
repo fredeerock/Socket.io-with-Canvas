8510be73b8eaccd4c909924c53364388fa5d2ae2
Derick Ostrenko <dostrenko@gmail.com>
2014-06-23 16:31:24 -0500

    sending mouse to canvas

--- public/js/viz.js
+++ public/js/viz.js
@@ -8,8 +8,10 @@ var ctx = canvas.getContext("2d");
 var mx;
 var my;
 
-canvas.addEventListener('mousemove', getMousePos, false);
 window.addEventListener('resize', setup, false );
+canvas.addEventListener('mousemove', getMousePos, false);
+window.addEventListener('touchmove', getMousePos, false);
+
 
 window.onload = function() {
 	setup();
@@ -19,12 +21,14 @@ window.onload = function() {
 function getMousePos(event) {
 	mx = event.clientX;
 	my = event.clientY;
+	drawMouse();
+	socket.emit('from client', { clientMouseX: mx, clientMouseY: my });
 }
 
 function drawMouse() {
 	ctx.beginPath();
+	ctx.fillStyle = "rgba(255, 255, 255, 1)";
 	ctx.arc(mx, my, 50, 0, 2*Math.PI);
-	ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
 	ctx.fill();
 }
 
@@ -34,12 +38,18 @@ function setup(){
 }
 
 function draw(){
-	drawMouse();
-	ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-	requestAnimationFrame(draw);
-
-	ctx.fillStyle = "rgba(0, 0, 0, 0.5)"
+	//Draw background
+	ctx.fillStyle = "rgba(10, 10, 10, 0.05)"
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	
+	//Draw boards
+	ctx.beginPath();
+	ctx.moveTo(canvas.width/2,0);
+	ctx.lineTo(canvas.width/2,canvas.height);
+	ctx.strokeStyle = "rgba(255, 255, 255, 1)";
+	ctx.stroke();
+
+	requestAnimationFrame(draw);
 }
 
 
