4d00afd15a5158fd3ce9f8f984c1ed7f7c807c84
Derick Ostrenko <dostrenko@gmail.com>
2014-06-23 15:44:04 -0500

    Basics done

--- public/js/viz.js
+++ public/js/viz.js
@@ -8,9 +8,8 @@ var ctx = canvas.getContext("2d");
 var mx;
 var my;
 
-canvas.addEventListener('mousemove', drawMouse, false);
+canvas.addEventListener('mousemove', getMousePos, false);
 window.addEventListener('resize', setup, false );
-
 
 window.onload = function() {
 	setup();
@@ -20,28 +19,27 @@ window.onload = function() {
 function getMousePos(event) {
 	mx = event.clientX;
 	my = event.clientY;
-	
-	console.log("mx: " + mx + " my:" + my);
-
 }
 
 function drawMouse() {
 	ctx.beginPath();
-	ctx.arc(event.clientX, event.clientY,50,0,2*Math.PI);
+	ctx.arc(mx, my, 50, 0, 2*Math.PI);
 	ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
 	ctx.fill();
-
 }
 
 function setup(){
 	canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-	ctx.fillRect(0,0,canvas.width, canvas.height);
 }
 
 function draw(){
+	drawMouse();
 	ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
 	requestAnimationFrame(draw);
+
+	ctx.fillStyle = "rgba(0, 0, 0, 0.5)"
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 
